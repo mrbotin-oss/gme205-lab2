902bd12903e31b0c244400b274a538a93ccfe78c
mrbotin-oss <mrbotin@up.edu.ph>
2026-02-14 02:53:31 +0800

    edit

--- src/demo.py
+++ src/demo.py
@@ -1,15 +1,16 @@
-from spatial import Point
+##from spatial import Point
 
-p = Point ("A", 121.0, 14.6)
-print(p.id, p.lon, p.lat)
-print(p.to_tuple())
+##p = Point ("A", 121.0, 14.6)
+##print(p.id, p.lon, p.lat)
+##print(p.to_tuple())
 
-distance = Point (121, 14.6, 90, 15)
-print(distance)  # m
+##distance = Point (121, 14.6, 90, 15)
+##print(distance)  # m
 
-a = Point ("B", 122.0, 13.6)
-print(p.distance_to(a))
+##a = Point ("B", 122.0, 13.6)
+##print(p.distance_to(a))
 
+### Part C. Designing a Spatial Collection: PointSet (The Challenge) 
 
 from spatial import PointSet
 
@@ -19,8 +20,8 @@ def main():
     print("Point count:", points.count())
     print("Bounding box:", points.bbox())
 
-    tagged_points = points.filter_by_tag("restaurant")
-    print("Restaurant points:", tagged_points.count())
+    tagged_points = points.filter_by_tag("poi")
+    print("poi points:", tagged_points.count())
 
 if __name__ == "__main__":
     main()
